99915c75c13b1f968c2601066ba114b261f482ec
Duong <duongptryu@gmail.com>
2021-10-07 21:55:49 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'asm',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
